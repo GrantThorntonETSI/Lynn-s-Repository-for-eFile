484a0bbf02cd4dabf0d2cea9470f577e0247b2d7
Lynn Stanikmas <lstanikmas@gmail.com>
2019-05-21 22:21:15 -0400

    removal
    delete outdated proof files

--- proofs/019_0315_proofs/owner partnership files/js/sole.js
+++
@@ -1,7 +0,0 @@
-var link = document.querySelector('link[rel="import"]#sole');
-
-// Clone the <template> in the import.
-var template = link.import.querySelector('template');
-var clone = document.importNode(template.content, true);
-
-document.querySelector('#container').appendChild(clone);
